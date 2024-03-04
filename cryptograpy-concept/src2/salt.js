974aba0e114a5fad9793d66333b9f9309cb8b32d
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2024-03-04 14:40:44 +0600

    Update salt.js

--- cryptograpy-concept/src2/salt.js
+++ cryptograpy-concept/src2/salt.js
@@ -1,32 +1,38 @@
-
-const {scryptSync, randomBytes, timingSafeEqual} = require('crypto')
+import {scryptSync, randomBytes, timingSafeEqual} from 'crypto'
 
 const users = []
-
-function signUp(email, password){
+function signup(email, password){
+    // generat a random salt and convert it to hexadecimal string
     const salt = randomBytes(16).toString('hex')
+    // hash the password
     const hashedPassword = scryptSync(password, salt, 64).toString('hex')
 
-    const user = {email: email, hash: hashedPassword, salt: salt}
+    const user = {email, password: `${salt}:${hashedPassword}`}
     users.push(user)
+
     return user
 }
 
 function login(email, password){
-    const user = users.find(user => user.email === email)
-    const {salt, hash} = user
+    // retrieve user by email
+    const user = users.find(v =>v.email === email)
+    // get the salt and hashed password from db
+    const [salt, key] = user.password.split(":")
+    // hash the given password with the salt(retreived from db)
     const hashedBuffer = scryptSync(password, salt, 64)
 
-    const keyBuffer = Buffer.from(hash, 'hex')
-    const match = timingSafeEqual(hashedBuffer, keyBuffer)
+    //  convert the stored key(hashed password) from hexadecimal to buffer
+    const keyBufffer = Buffer.from(key, 'hex')
+    // it is used to determine whether two variables are equal without exposing timing information that may allow an attacker to guess one of the values.
+    const match = timingSafeEqual(hashedBuffer, keyBufffer)
 
     if (match){
-        console.log("login success!")
-    }
-    else {
-        console.log("login Failed")
+        console.log('login success!')
+    } else {
+        console.log("logn fail")
     }
 }
 
-signUp("bob@mail.com", "10101010")
-login("bob@mail.com", "10101010")
+const user = signup('win@mail.com', "password123")
+login('win@mail.com', "password123")
+console.log(user)
